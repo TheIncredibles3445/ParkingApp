49eca6cd2929d28a0b8090483e3fae1b4e1807a5
asgadabdallahabdallah1 <59683364+asgadabdallahabdallah1@users.noreply.github.com>
2020-04-02 20:47:14 +0300

    resolving db

--- db.js
+++ db.js
@@ -3,14 +3,22 @@ import "firebase/firestore";
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyBe9tI4xIee_swg9hU6MX29CARfIQNn2GE",
-  authDomain: "parkingapp-a8d89.firebaseapp.com",
-  databaseURL: "https://parkingapp-a8d89.firebaseio.com",
-  projectId: "parkingapp-a8d89",
-  storageBucket: "parkingapp-a8d89.appspot.com",
-  messagingSenderId: "683974531069",
-  appId: "1:683974531069:web:9453d0463e8c4f53cceb94",
-  measurementId: "G-EEWDWG338L"
+  apiKey: "AIzaSyAxgkqz46PpA_pT9beQphwB_B55kurYVa8",
+  authDomain: "parkingapp-38277.firebaseapp.com",
+  databaseURL: "https://parkingapp-38277.firebaseio.com",
+  projectId: "parkingapp-38277",
+  storageBucket: "parkingapp-38277.appspot.com",
+  messagingSenderId: "657425654700",
+  appId: "1:657425654700:web:4b07f82307f20539c81009",
+  measurementId: "G-DWVWXL4E8P"
+  // apiKey: "AIzaSyAQTaHbGrvP6mEMLiTBVzmDL6QOYs0IHFE",
+  // authDomain: "messages-c612c.firebaseapp.com",
+  // databaseURL: "https://messages-c612c.firebaseio.com",
+  // projectId: "messages-c612c",
+  // storageBucket: "messages-c612c.appspot.com",
+  // messagingSenderId: "537953148662",
+  // appId: "1:537953148662:web:db97903171c3a2e9e996d7",
+  // measurementId: "G-FXFQQRS54D"
 });
 //firebase.functions()
 export default firebase.firestore();
