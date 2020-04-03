b26187a4c4d3faafc5ee91eabdde5b13b61f3f40
TheIncredibles3445 <62172317+TheIncredibles3445@users.noreply.github.com>
2020-04-03 22:44:16 +0300

    pushing new db

--- db.js
+++ db.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyCNs3ICBXqvnOHf9SP05LwfxZnXM2CXFrg",
-  authDomain: "parking-app-3b592.firebaseapp.com",
-  databaseURL: "https://parking-app-3b592.firebaseio.com",
-  projectId: "parking-app-3b592",
-  storageBucket: "parking-app-3b592.appspot.com",
-  messagingSenderId: "167019920679",
-  appId: "1:167019920679:web:848574c0f0a6bc3772bd02",
-  measurementId: "G-TYS24KVHHM"
+  apiKey: "AIzaSyAipzJN8DJ_GIae8963KsLJ0U2goa4VL34",
+  authDomain: "parking-assistant-d2d25.firebaseapp.com",
+  databaseURL: "https://parking-assistant-d2d25.firebaseio.com",
+  projectId: "parking-assistant-d2d25",
+  storageBucket: "parking-assistant-d2d25.appspot.com",
+  messagingSenderId: "312947533833",
+  appId: "1:312947533833:web:850e4a0de0950af12830d8",
+  measurementId: "G-7ZEZD60X58"
 });
 //firebase.functions()
 export default firebase.firestore();
